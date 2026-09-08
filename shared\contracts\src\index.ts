561766b3aa5808b6ec2f3002242943878e5b5a83
Zeshan Zhang <142085030+zhangmask@users.noreply.github.com>
2026-09-08 11:07:58 +0800

    delete backslash path shared\contracts\src\index.ts

--- shared\contracts\src\index.ts
+++
@@ -1,6 +0,0 @@
-/** @sim/contracts 统一导出（唯一真源，其它包只 import 这里） */
-export * from './errors.ts';
-export * from './sse.ts';
-export * from './topic.ts';
-export * from './game.ts';
-export * from './assets.ts';
